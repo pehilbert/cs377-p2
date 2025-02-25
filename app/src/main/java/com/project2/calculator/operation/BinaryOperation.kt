a540ed48311b7f197282299b26164274605ed0ac
pehilbert <pehilbert03@gmail.com>
2025-02-25 13:22:12 -0700

    Adds comments to operations

--- app/src/main/java/com/project2/calculator/operation/BinaryOperation.kt
+++ app/src/main/java/com/project2/calculator/operation/BinaryOperation.kt
@@ -1,6 +1,10 @@
 package com.project2.calculator.operation
 
+// Interface for various binary operations (operations involving two numbers)
 interface BinaryOperation {
+    // calculates the operation given the two numbers
     fun calculate(a : Double, b : Double) : Double
+
+    // provides the symbol of the operation (i.e. '+')
     fun getSymbol() : String
 }
--- app/src/main/java/com/project2/calculator/operation/UnaryOperation.kt
+++ app/src/main/java/com/project2/calculator/operation/UnaryOperation.kt
@@ -1,6 +1,10 @@
 package com.project2.calculator.operation
 
+// interface for unary operations (operations with only one number)
 interface UnaryOperation {
+    // calculates the operation given the number
     fun calculate (x : Double) : Double
+
+    // gets string representing the operation that was done (i.e. "sqrt(5)")
     fun getNotation(numberStr : String) : String
 }
